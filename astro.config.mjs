39f564cd5a281ec1f4fe9c3242585a37b07fe59c
TosattoSimonePio <tosatto.simonepio@gmail.com>
2023-12-05 19:18:53 +0100

    updated references for the deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://Simo56.github.io',
-  	//base: '/MyPortfolio',
-	//public: '/public',
+  	base: '/MyPortfolio',
 	integrations: [tailwind(), mdx(), image()],
 });
